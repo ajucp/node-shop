93c018b04e1ce11e82e3f8850f85b23aa6bae7ff
ajucp <ajmalcp4@gmail.com>
2024-10-10 16:01:39 +0530

    clear the file

--- mongoose.js
+++ mongoose.js
@@ -21,11 +21,10 @@ const helmet=require('helmet');//import helmet for securing the headers
 const compression=require('compression');//to compress the page size of any thing in the web
 const morgan=require('morgan');     //for logging
 
-// const MOngoDB_URI=`mongodb+srv://ajucp:5WZifQn3iwl4oHqx@cluster0.98nsj.mongodb.net/node-shop?retryWrites=true&w=majority`;
-// const MOngoDB_URI = `mongodb+srv://ajucp:5WZifQn3iwl4oHqx@cluster0.98nsj.mongodb.net/node-shop?retryWrites=true&w=majority`;
-// const MOngoDB_URI=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.98nsj.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const MOngoDB_URI = `mongodb+srv://ajmalcp:AuJT5T4gmA4kLiNT@cluster0.mongodb.net/node-shop?retryWrites=true&w=majority`;
+const MOngoDB_URI=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.98nsj.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+
 
 
 const app=express();
